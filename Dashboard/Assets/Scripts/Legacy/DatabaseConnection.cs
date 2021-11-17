bfbbd9f2156d00f12873c3953913582aa564d585
Vlad <miu.vlad00@gmail.com>
2021-11-17 15:00:30 +0200

    starting mongoDB implementation

---
+++ Dashboard/Assets/Scripts/Legacy/DatabaseConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Realms.Sync;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        start();
+    }
+
+    private async Task start() //should this rly be asynk?
+    {
+        var myRealmAppId = "vulcom-vxnqd";
+        var app = App.Create(myRealmAppId);
+        var user = await app.LogInAsync(Credentials.Anonymous());
+    }
+}
