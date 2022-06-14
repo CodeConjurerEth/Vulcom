6f519520de3ddf0965381eb00d6c0ca0a1ac5b7f
CodeConjurerEth <c2wbob@protonmail.com>
2022-06-14 08:20:35 +0300

    ui changes

--- Dashboard/Assets/Scripts/Controller/AmestecController.cs
+++ Dashboard/Assets/Scripts/Controller/AmestecController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Realms;
-using Realms.Sync;
+//using Realms;
+//using Realms.Sync;
 using UnityEngine;
 using TMPro;
 using UnityEditor.SearchService;
